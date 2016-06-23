5e3ec8eacf608c58e302cac15e6552e26738d984
宙生上村 <uemurachuusei@uemura-no-macbook-pro.local>
2016-06-23 12:07:19 +0900

    始め

---
+++ FlappyBird/GameScene.swift
@@ -0,0 +1,13 @@
+//
+//  GameScene.swift
+//  FlappyBird
+//
+//  Created by 上村 宙生 on 2016/06/23.
+//  Copyright © 2016年 huemura. All rights reserved.
+//
+
+import SpriteKit
+
+class GameScene: SKScene {
+    
+}
